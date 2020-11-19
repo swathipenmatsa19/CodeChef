071940ead2b9cf623fedc7c2b9483ce1cd626d2a
Swathi Penmatsa <74686794+swathipenmatsa19@users.noreply.github.com>
2020-11-18 21:56:41 -0600

    RPD

---
+++ EasyMath/Codechef.java
@@ -0,0 +1,105 @@
+/*
+
+
+Chef is attending math classes. On each day, the teacher gives him homework. Yesterday, the teacher gave Chef a sequence of positive integers and asked him to find the maximum product of two different elements of this sequence. This homework was easy for Chef, since he knew that he should select the biggest two numbers.
+
+However, today, the homework is a little bit different. Again, Chef has a sequence of positive integers A1,A2,…,AN
+, but he should find two different elements of this sequence such that the sum of digits (in base 10
+) of their product is maximum possible.
+
+Chef thought, mistakenly, that he can still select the two largest elements and compute the sum of digits of their product. Show him that he is wrong by finding the correct answer ― the maximum possible sum of digits of a product of two different elements of the sequence A
+.
+
+Input
+The first line of the input contains a single integer T
+ denoting the number of test cases. The description of T
+ test cases follows.
+The first line of the input contains a single integer N
+.
+The second line contains N
+ space-separated integers A1,A2,…,AN
+.
+Output
+For each test case, print a single line containing one integer ― the maximum sum of digits.
+
+Constraints
+1≤T≤100
+2≤N≤100
+1≤Ai≤104
+ for each valid i
+Subtasks
+Subtask #1 (100 points): original constraints
+
+Example Input
+3
+2
+2 8
+3 
+8 2 8
+3
+9 10 11
+Example Output
+7
+10
+18
+
+
+*/
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+        while(n != 0) {
+            int r = n % 10;
+            sum += r;
+            n = n / 10;
+        }
+        return sum;
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- != 0) {
+            int max = 0;
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for(int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            for(int i = 0; i < n - 1; i++) {
+                for(int j = i + 1; j < n; j++) {
+                    int prod = a[i] * a[j];
+                    int sum = sumOfDigits(prod);
+                    if(sum > max)
+                        max = sum;
+                }
+            }
+            System.out.println(max);
+        }// your code goes here
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
