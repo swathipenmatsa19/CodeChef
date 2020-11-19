45f37a3f495e3711a6a3edadda73ea6bde188113
Swathi Penmatsa <74686794+swathipenmatsa19@users.noreply.github.com>
2020-11-18 21:21:08 -0600

    START01

---
+++ NumberMirror/Codechef.java
@@ -0,0 +1,47 @@
+/*
+
+
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 ≤ n ≤ 105
+Sample Input
+123
+Sample Output
+123
+
+
+*/
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt());// your code goes here
+	}
+}
+
+
+
+
+
+
+
+
+
